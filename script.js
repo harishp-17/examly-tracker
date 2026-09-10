7261dd10f9f4da75a37e4e72bdfba02346071c50
harishp-17 <phari9074@gmail.com>
2026-09-10 15:38:08 +0530

    Update SCRIPT_URL and fix formatting issues

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxwW3coAMZZLNWnkZ9-jwSCCNej2NgK0lT7ZrKRIZNj0CW1-ho8E7KCDWbk9jn6COUj/exec";
 
 document.addEventListener("DOMContentLoaded", function() {
   const options = { year: 'numeric', month: 'short', day: 'numeric', weekday: 'short' };
